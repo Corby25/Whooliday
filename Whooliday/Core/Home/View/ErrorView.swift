54e03ab6eadd4a2798f7e8f3c70f707a4e8ed3df
theblackoreo <fabio.tagliani03@universitadipavia.it>
2024-07-08 22:25:40 +0200

    update HomeViewModel for testing

---
+++ Whooliday/Core/Home/View/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  Whooliday
+//
+//  Created by Fabio Tagliani on 08/07/24.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorView()
+}
